298839b7bedd42e35b079a791181aee685c5bde6
Cochenille <drolet.laurent@carrefour.cegepvicto.ca>
2020-08-18 15:26:12 -0400

    service worker remove cache

--- PointNClick/service-worker.js
+++ PointNClick/service-worker.js
@@ -1,6 +1,6 @@
 
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
+const CACHE_NAME = 'static-cache-v2';
 
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
