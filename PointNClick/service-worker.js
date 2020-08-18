aed94c922158afd10bf816b9bdd09aa765b1e525
Cochenille <drolet.laurent@carrefour.cegepvicto.ca>
2020-08-18 15:09:50 -0400

    service worker fix 3

--- PointNClick/service-worker.js
+++ PointNClick/service-worker.js
@@ -6,17 +6,17 @@ const CACHE_NAME = 'static-cache-v1';
 const FILES_TO_CACHE = [
     'offline.html',
 ];
-// CODELAB: Precache static resources here.
-evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-        console.log('[ServiceWorker] Pre-caching offline page');
-        return cache.addAll(FILES_TO_CACHE);
-    })
-);
+
 
 self.addEventListener('install', (evt) => {
     console.log('[ServiceWorker] Install');
     // Precache static resources here.
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('[ServiceWorker] Pre-caching offline page');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
     self.skipWaiting();
 });
 
