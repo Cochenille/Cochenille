9108b714b89d27b7f742a2b82c79e9f56fc4810e
Cochenille <drolet.laurent@carrefour.cegepvicto.ca>
2020-08-20 21:38:36 -0400

    ajout du bouton install

--- PointNClick2/service-worker.js
+++ PointNClick2/service-worker.js
@@ -1,5 +1,5 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v5';
+const CACHE_NAME = 'static-cache-v6';
 
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
