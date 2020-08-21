06cbeecd4ae86b81634a8fd874509da516d4c831
Cochenille <drolet.laurent@carrefour.cegepvicto.ca>
2020-08-20 20:15:13 -0400

    ajout du service worker fonctionnel fix offline

--- PointNClick2/service-worker.js
+++ PointNClick2/service-worker.js
@@ -53,7 +53,7 @@ self.addEventListener('fetch', (evt) => {
             .catch(() => {
                 return caches.open(CACHE_NAME)
                     .then((cache) => {
-                        return cache.match('/Cochenille/PointNClick/offline.html' );
+                        return cache.match('/Cochenille/PointNClick2/offline.html' );
                     });
             })
     );
