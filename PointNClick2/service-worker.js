271cd9cd9448daefc8df6db8e0995dcc7935484b
Cochenille <drolet.laurent@carrefour.cegepvicto.ca>
2020-08-29 08:28:15 -0400

    test offline 6

--- PointNClick2/service-worker.js
+++ PointNClick2/service-worker.js
@@ -1,5 +1,5 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v12';
+const CACHE_NAME = 'static-cache-v13';
 
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
