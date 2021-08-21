e3ff01246ac858a85e0ed3eb4df8112bd6c59103
Cochenille <drolet.laurent@carrefour.cegepvicto.ca>
2021-08-21 09:32:03 -0400

    ajout de la suppression de cache

--- PointNClickNoPWA/service-worker.js
+++ PointNClickNoPWA/service-worker.js
@@ -1,5 +1,5 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
+const CACHE_NAME = 'static-cache-v2';
 
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
@@ -25,6 +25,17 @@ self.addEventListener('install', (evt) => {
 self.addEventListener('activate', (evt) => {
    console.log('[ServiceWorker] Activate');
    //Remove previous cached data from disk.
+
+   evt.waitUntil(
+      caches.keys().then((keyList) => {
+          return Promise.all(keyList.map((key) => {
+              if (key !== CACHE_NAME) {
+                  console.log('[ServiceWorker] Removing old cache', key);
+                  return caches.delete(key);
+              }
+          }));
+      })
+  );
 
    self.clients.claim();
 });
