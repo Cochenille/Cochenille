b06ef873341463544cc5065106589ca4742c5d25
Cochenille <drolet.laurent@carrefour.cegepvicto.ca>
2021-08-21 11:10:05 -0400

    test2

--- PointNClickNoPWA/service-worker.js
+++ PointNClickNoPWA/service-worker.js
@@ -1,5 +1,5 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v6';
+const CACHE_NAME = 'static-cache-v7';
 
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
@@ -8,7 +8,7 @@ const FILES_TO_CACHE = [
    "histoire.html",
    "jeux.html",
    "install.js",
-   "/img"
+   "img/*"
 ];
 
 self.addEventListener('install', (evt) => {
